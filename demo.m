7190c4f0e5aa4451aa356683d78e48d47475fb9a
Minchao <logic.mcwu@gmail.com>
2017-09-28 13:13:51 -0500

    fix OutputFormat

--- demo.m
+++ demo.m
@@ -1,22 +1,17 @@
 OutputFormat[i_Integer] := "I[" <> ToString[i] <> "]"
-
 OutputFormat[s_String] := "T[\"" <> s <> "\"]"
-
 OutputFormat[s_Symbol] := "Y[" <> ToString[s] <> "]"
-
 OutputFormat[h_[args___]] := 
- "A" <> OutputFormat[h] <> "[" <> 
-  StringRiffle[Map[OutputFormat, List[args]], ","] <> "]"
+  "A" <> OutputFormat[h] <> "[" <> 
+    StringRiffle[Apply[List, Map[OutputFormat, Hold[args]]], ","] <> 
+  "]"
 
- 
+SetAttributes[OutputFormat, HoldFirst];
   
-
 RunLeanTactic[x_, t_String, b_?BooleanQ]:=Module[{s,cmd}, s=OpenWrite["temp.lean", CharacterEncoding -> "UTF8"]; cmd=StringForm["run_cmd `1` \"`2`\" `3` >>= write_string",t,x // OutputFormat, If[b, "tt", "ff"]]; WriteString[s, "import demo", "\n", cmd]; Close[s];RunThrough["lean temp.lean", 0];ReadString["temp.txt"]];
-                                                                                           
 
 RunLeanTactic[x_,t_]:=RunLeanTactic[x,t,False]
-                                                                                                                                  
-                                                                                                                                 ProveUsingLeanTactic[x_, t_String, b_?BooleanQ] :=
-                                                       Module[{s,cmd}, s=OpenWrite["temp.lean", CharacterEncoding -> "UTF8"]; cmd=StringForm["run_cmd prove_using_tac (`1`) \"`2`\" `3` >>= write_string",t,x // OutputFormat, If[b, "tt", "ff"]]; WriteString[s, "import demo", "\n", cmd]; Close[s];RunThrough["lean temp.lean", 0];ReadString["temp.txt"]];
+
+ProveUsingLeanTactic[x_, t_String, b_?BooleanQ] := Module[{s,cmd}, s=OpenWrite["temp.lean", CharacterEncoding -> "UTF8"]; cmd=StringForm["run_cmd prove_using_tac (`1`) \"`2`\" `3` >>= write_string",t,x // OutputFormat, If[b, "tt", "ff"]]; WriteString[s, "import demo", "\n", cmd]; Close[s];RunThrough["lean temp.lean", 0];ReadString["temp.txt"]];
 
 ProveUsingLeanTactic[x_,t_] := ProveUsingLeanTactic[x,t,False]
