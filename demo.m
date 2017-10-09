748f0cd2146ecc811a281278e990678bbf9cb603
Minchao <logic.mcwu@gmail.com>
2017-10-09 16:00:19 -0500

    fix issues with encodings

--- demo.m
+++ demo.m
@@ -6,14 +6,14 @@ OutputFormat[h_[args___]] :=
     StringRiffle[Apply[List, Map[OutputFormat, Hold[args]]], ","] <> 
   "]"
   
-RunLeanTactic[x_, t_String, b_?BooleanQ]:=Module[{s,cmd}, s=OpenWrite["temp.lean", CharacterEncoding -> "UTF8"]; cmd=StringForm["run_cmd `1` \"`2`\" `3` >>= write_string",t,x // OutputFormat, If[b, "tt", "ff"]]; WriteString[s, "import demo", "\n", cmd]; Close[s];RunThrough["lean temp.lean", 0];ReadString["temp.txt"]];
+RunLeanTactic[x_, t_String, b_?BooleanQ]:=Module[{s,cmd}, s=OpenWrite["temp.lean", CharacterEncoding -> "UTF8"]; cmd=StringForm["run_cmd `1` \"`2`\" `3` >>= write_string",t,x // OutputFormat, If[b, "tt", "ff"]]; WriteString[s, "import demo", "\n", cmd]; Close[s];RunThrough["lean temp.lean", 0];Import["temp.txt", CharacterEncoding -> "UTF8"]];
 
 RunLeanTactic[x_,t_]:=RunLeanTactic[x,t,False]
 
 SetAttributes[OutputFormat, HoldFirst];
 SetAttributes[RunLeanTactic, HoldFirst];
 
-ProveUsingLeanTactic[x_, t_String, b_?BooleanQ] := Module[{s,cmd}, s=OpenWrite["temp.lean", CharacterEncoding -> "UTF8"]; cmd=StringForm["run_cmd prove_using_tac (`1`) \"`2`\" `3` >>= write_string",t,x // OutputFormat, If[b, "tt", "ff"]]; WriteString[s, "import demo", "\n", cmd]; Close[s];RunThrough["lean temp.lean", 0];ReadString["temp.txt"]];
+ProveUsingLeanTactic[x_, t_String, b_?BooleanQ] := Module[{s,cmd}, s=OpenWrite["temp.lean", CharacterEncoding -> "UTF8"]; cmd=StringForm["run_cmd prove_using_tac (`1`) \"`2`\" `3` >>= write_string",t,x // OutputFormat, If[b, "tt", "ff"]]; WriteString[s, "import demo", "\n", cmd]; Close[s];RunThrough["lean temp.lean", 0];Import["temp.txt", CharacterEncoding -> "UTF8"]];
 
 ProveUsingLeanTactic[x_,t_] := ProveUsingLeanTactic[x,t,False]
 
